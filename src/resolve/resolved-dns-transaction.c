4179328a5204a3149e16a4a46796c389b97e7c7d
Tom Gundersen <teg@jklm.no>
2015-07-06 16:48:24 +0200

    resolved: announce support for large UDP packets
    This is often needed for proper DNSSEC support, and even to handle AAAA records
    without falling back to TCP.
    
    If the path between the client and server is fully compliant, this should always
    work, however, that is not the case, and overlarge packets will get mysteriously
    lost in some cases.
    
    For that reason, we use a similar fallback mechanism as we do for palin EDNS0,
    EDNS0+DO, etc.:
    
    The large UDP size feature is different from the other supported feature, as we
    cannot simply verify that it works based on receiving a reply (as the server
    will usually send us much smaller packets than what we claim to support, so
    simply receiving a reply does not mean much).
    
    For that reason, we keep track of the largest UDP packet we ever received, as this
    is the smallest known good size (defaulting to the standard 512 bytes). If
    announcing the default large size of 4096 fails (in the same way as the other
    features), we fall back to the known good size. The same logic of retrying after a
    grace-period applies.

--- src/resolve/resolved-dns-transaction.c
+++ src/resolve/resolved-dns-transaction.c
@@ -375,11 +375,19 @@ void dns_transaction_process_reply(DnsTransaction *t, DnsPacket *p) {
 
                         return;
                 } else {
-                        if (t->current_features >= t->server->verified_features)
-                                t->server->verified_features = t->current_features;
+                        if (t->current_features == DNS_SERVER_FEATURE_LEVEL_LARGE)
+                                /* even if we successfully receive a reply to a request announcing
+                                   support for large packets, that does not mean we can necessarily
+                                   receive large packets. We verify the max size of UDP packets we
+                                   can receive separately */
+                                t->server->verified_features = DNS_SERVER_FEATURE_LEVEL_LARGE - 1;
+                        else {
+                                if (t->current_features >= t->server->verified_features)
+                                        t->server->verified_features = t->current_features;
 
-                        if (t->current_features == t->server->possible_features)
-                                t->server->n_failed_attempts = 0;
+                                if (t->current_features == t->server->possible_features)
+                                        t->server->n_failed_attempts = 0;
+                        }
                 }
         }
 
@@ -423,6 +431,12 @@ void dns_transaction_process_reply(DnsTransaction *t, DnsPacket *p) {
         if (!dns_question_is_superset(p->question, t->question) ||
             !dns_question_is_superset(t->question, p->question))
                 dns_transaction_complete(t, DNS_TRANSACTION_INVALID_REPLY);
+
+        if (t->server)
+                /* Remember the size of the largest UDP packet we received from a server,
+                   we know that we can always announce support for packets with this size */
+                if (t->server->received_udp_packet_max < p->size)
+                        t->server->received_udp_packet_max = p->size;
 
         /* According to RFC 4795, section 2.9. only the RRs from the answer section shall be cached */
         dns_cache_put(&t->scope->cache, p->question, DNS_PACKET_RCODE(p), p->answer, DNS_PACKET_ANCOUNT(p), 0, p->family, &p->sender);
